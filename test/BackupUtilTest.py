b11a68fba442bec498fe9d920e0348f566742637
InMath <the_wuying@qq.com>
2015-06-17 16:06:55 +0800

    修改了test代码

--- test/BackupUtilTest.py
+++ test/BackupUtilTest.py
@@ -1,4 +1,4 @@
 # -- coding=utf8 --
 __author__ = 'Administrator'
-import BackupUtil as util
-util.sendmail("你们好啊！54321987899", "inmath@126.com", ["the_wuying@qq.com"])
+import Iemail.tools as util
+util.sendmail("你们好啊！54321987899", "inmath@126.com", ["the_wuying@qq.com", "weicheng7452@qq.com"])
